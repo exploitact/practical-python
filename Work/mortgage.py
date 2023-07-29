d0de35d08241a02b5d93a3f8262ecbd2a4ddd41a
exploitact <forkaiser@gmail.com>
2023-07-29 00:35:53 -0700

    finished exercise 1.7

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,24 @@
 # mortgage.py
 #
 # Exercise 1.7
+# mortgage.py
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+total_month = 0
+payment_extra= 3684.11
+i = 0
+
+while principal > 0:
+    principal = principal * (1+rate/12) - payment
+    total_paid = total_paid + payment
+    total_month +=1  
+    while i < 12:
+        principal = principal * (1+rate/12) - payment_extra
+        total_paid = total_paid + payment_extra
+        i +=1
+        total_month +=1 
+   
+print('Total paid', total_paid, "over", total_month, "months")
