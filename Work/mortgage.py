1588daeaf997540e6e18ea4c3c85af99863a13e8
exploitact <forkaiser@gmail.com>
2023-07-29 16:00:18 -0700

    Exercise 1.10

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -8,17 +8,21 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 total_month = 0
-payment_extra= 3684.11
-i = 0
+
+extra_payment_start_month = int(input("Enter the Extra Payment Start Month ="))
+extra_payment_end_month = int(input("Enter the Extra Payment End Month ="))
+extra_payment = float(input("Enter the Extra Payment amount ="))
 
 while principal > 0:
-    principal = principal * (1+rate/12) - payment
+    principal = principal * (1 + rate / 12) - payment
     total_paid = total_paid + payment
-    total_month +=1  
-    while i < 12:
-        principal = principal * (1+rate/12) - payment_extra
-        total_paid = total_paid + payment_extra
-        i +=1
-        total_month +=1 
-   
-print('Total paid', total_paid, "over", total_month, "months")
+    total_month += 1
+    while extra_payment_start_month < extra_payment_end_month:
+        principal = principal * (1 + rate / 12) - (extra_payment + payment)
+        total_paid = total_paid + (extra_payment + payment)
+        extra_payment_start_month += 1
+        total_month += 1
+
+    print(total_month, total_paid, principal)
+    
+print('Total paid:', total_paid, "over", total_month, "months")
